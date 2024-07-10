aa25c1acaa0d2e11d8dd6cbe879c78bcba64a993
Jacob Lindgren <jacob@anedot.com>
2024-07-10 16:14:32 -0500

    add specs for api clients

--- spec/api/balance_transactions_api_spec.rb
+++ spec/api/balance_transactions_api_spec.rb
@@ -2,18 +2,35 @@ require 'spec_helper'
 require 'json'
 
 describe 'BalanceTransactionsApi' do
-  before do
-    # run before each test
-    @api_instance = BckbnPay::BalanceTransactionsApi.new
+  let(:api_instance) { BckbnPay::BalanceTransactionsApi.new }
+  let(:config) do
+    api_instance.api_client.config.tap do |c|
+      c.access_token = Faker::Lorem.word
+      c.host = Faker::Internet.domain_name
+      c.scheme = 'http'
+    end
   end
-
-  after do
-    # run after each test
+  let(:path) { '/balance_transactions' }
+  let(:request_headers) do
+    {
+      'Accept'        =>  'application/json',
+      "Authorization" => "Bearer #{config.access_token}",
+      "Content-Type"  => "application/json",
+      "Expect" => '',
+      'User-Agent' => 'OpenAPI-Generator/0.1.0/ruby',
+      'X-Api-Version' => '0.1.0'
+    }
+  end
+  let(:response_headers) do
+    {
+      'Accept'        =>  'application/json',
+      "Content-Type"  => "application/json"
+    }
   end
 
   describe 'test an instance of BalanceTransactionsApi' do
     it 'should create an instance of BalanceTransactionsApi' do
-      expect(@api_instance).to be_instance_of(BckbnPay::BalanceTransactionsApi)
+      expect(api_instance).to be_instance_of(BckbnPay::BalanceTransactionsApi)
     end
   end
 
@@ -24,8 +41,22 @@ describe 'BalanceTransactionsApi' do
   # @param [Hash] opts the optional parameters
   # @return [BalanceTransactionsGet200Response]
   describe 'balance_transactions_get test' do
+    before do
+      stub_request(:get, [config.host, path].join)
+      .with(headers: request_headers)
+      .to_return(
+        body: fixture("balance_transactions/list_200.json"),
+        headers: response_headers,
+        status: 200
+      )
+    end
+
     it 'should work' do
-      # assertion here. ref: https://rspec.info/features/3-12/rspec-expectations/built-in-matchers/
+      res = api_instance.balance_transactions_get("0.1.0", "application/json")
+
+      expect(res.data).to be_a(Array)
+      expect(res.data.first).to be_a(BckbnPay::BalanceTransaction)
+      expect(a_request(:get, [config.host, path].join).with(headers: request_headers)).to have_been_made.once
     end
   end
 
