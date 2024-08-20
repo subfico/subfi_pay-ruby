2920e7b06cbd5e9d0870fcd204b61d06020351cf
Jacob Lindgren <jacob@anedot.com>
2024-08-20 14:33:24 -0500

    Add Refunds#(list|show) and Refund#cancel w/ specs

--- lib/bckbn_pay/api/refunds_api.rb
+++ lib/bckbn_pay/api/refunds_api.rb
@@ -19,6 +19,74 @@ module BckbnPay
     def initialize(api_client = ApiClient.default)
       @api_client = api_client
     end
+    # Cancel a refund
+    # @param x_api_version [String] 
+    # @param id [String] The ID of the refund to cancel.
+    # @param [Hash] opts the optional parameters
+    # @return [RefundResponse]
+    def cancel_refund(x_api_version, id, opts = {})
+      data, _status_code, _headers = cancel_refund_with_http_info(x_api_version, id, opts)
+      data
+    end
+
+    # Cancel a refund
+    # @param x_api_version [String] 
+    # @param id [String] The ID of the refund to cancel.
+    # @param [Hash] opts the optional parameters
+    # @return [Array<(RefundResponse, Integer, Hash)>] RefundResponse data, response status code and response headers
+    def cancel_refund_with_http_info(x_api_version, id, opts = {})
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: RefundsApi.cancel_refund ...'
+      end
+      # verify the required parameter 'x_api_version' is set
+      if @api_client.config.client_side_validation && x_api_version.nil?
+        fail ArgumentError, "Missing the required parameter 'x_api_version' when calling RefundsApi.cancel_refund"
+      end
+      # verify the required parameter 'id' is set
+      if @api_client.config.client_side_validation && id.nil?
+        fail ArgumentError, "Missing the required parameter 'id' when calling RefundsApi.cancel_refund"
+      end
+      # resource path
+      local_var_path = '/refunds/{id}/cancel'.sub('{' + 'id' + '}', CGI.escape(id.to_s))
+
+      # query parameters
+      query_params = opts[:query_params] || {}
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+      header_params[:'X-Api-Version'] = x_api_version
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:debug_body]
+
+      # return_type
+      return_type = opts[:debug_return_type] || 'RefundResponse'
+
+      # auth_names
+      auth_names = opts[:debug_auth_names] || ['bearerAuth']
+
+      new_options = opts.merge(
+        :operation => :"RefundsApi.cancel_refund",
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type
+      )
+
+      data, status_code, headers = @api_client.call_api(:PUT, local_var_path, new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: RefundsApi#cancel_refund\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+
     # Create a refund
     # @param x_api_version [String] 
     # @param refund_attributes [RefundAttributes] 
@@ -91,6 +159,146 @@ module BckbnPay
       data, status_code, headers = @api_client.call_api(:POST, local_var_path, new_options)
       if @api_client.config.debugging
         @api_client.config.logger.debug "API called: RefundsApi#create_refund\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+
+    # Get a refund by ID
+    # @param x_api_version [String] 
+    # @param id [String] The ID of the refund to retrieve.
+    # @param [Hash] opts the optional parameters
+    # @option opts [String] :sub_merchant_id Filter results by sub_merchant ID.
+    # @return [RefundResponse]
+    def get_refund(x_api_version, id, opts = {})
+      data, _status_code, _headers = get_refund_with_http_info(x_api_version, id, opts)
+      data
+    end
+
+    # Get a refund by ID
+    # @param x_api_version [String] 
+    # @param id [String] The ID of the refund to retrieve.
+    # @param [Hash] opts the optional parameters
+    # @option opts [String] :sub_merchant_id Filter results by sub_merchant ID.
+    # @return [Array<(RefundResponse, Integer, Hash)>] RefundResponse data, response status code and response headers
+    def get_refund_with_http_info(x_api_version, id, opts = {})
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: RefundsApi.get_refund ...'
+      end
+      # verify the required parameter 'x_api_version' is set
+      if @api_client.config.client_side_validation && x_api_version.nil?
+        fail ArgumentError, "Missing the required parameter 'x_api_version' when calling RefundsApi.get_refund"
+      end
+      # verify the required parameter 'id' is set
+      if @api_client.config.client_side_validation && id.nil?
+        fail ArgumentError, "Missing the required parameter 'id' when calling RefundsApi.get_refund"
+      end
+      # resource path
+      local_var_path = '/refunds/{id}'.sub('{' + 'id' + '}', CGI.escape(id.to_s))
+
+      # query parameters
+      query_params = opts[:query_params] || {}
+      query_params[:'sub_merchant_id'] = opts[:'sub_merchant_id'] if !opts[:'sub_merchant_id'].nil?
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+      header_params[:'X-Api-Version'] = x_api_version
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:debug_body]
+
+      # return_type
+      return_type = opts[:debug_return_type] || 'RefundResponse'
+
+      # auth_names
+      auth_names = opts[:debug_auth_names] || ['bearerAuth']
+
+      new_options = opts.merge(
+        :operation => :"RefundsApi.get_refund",
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type
+      )
+
+      data, status_code, headers = @api_client.call_api(:GET, local_var_path, new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: RefundsApi#get_refund\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+
+    # List refunds for a Charge
+    # @param x_api_version [String] 
+    # @param charge_id [String] The ID of the charge to which this refund belongs.
+    # @param [Hash] opts the optional parameters
+    # @return [ListRefundsResponse]
+    def list_refunds(x_api_version, charge_id, opts = {})
+      data, _status_code, _headers = list_refunds_with_http_info(x_api_version, charge_id, opts)
+      data
+    end
+
+    # List refunds for a Charge
+    # @param x_api_version [String] 
+    # @param charge_id [String] The ID of the charge to which this refund belongs.
+    # @param [Hash] opts the optional parameters
+    # @return [Array<(ListRefundsResponse, Integer, Hash)>] ListRefundsResponse data, response status code and response headers
+    def list_refunds_with_http_info(x_api_version, charge_id, opts = {})
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: RefundsApi.list_refunds ...'
+      end
+      # verify the required parameter 'x_api_version' is set
+      if @api_client.config.client_side_validation && x_api_version.nil?
+        fail ArgumentError, "Missing the required parameter 'x_api_version' when calling RefundsApi.list_refunds"
+      end
+      # verify the required parameter 'charge_id' is set
+      if @api_client.config.client_side_validation && charge_id.nil?
+        fail ArgumentError, "Missing the required parameter 'charge_id' when calling RefundsApi.list_refunds"
+      end
+      # resource path
+      local_var_path = '/refunds'
+
+      # query parameters
+      query_params = opts[:query_params] || {}
+      query_params[:'charge_id'] = charge_id
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+      header_params[:'X-Api-Version'] = x_api_version
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:debug_body]
+
+      # return_type
+      return_type = opts[:debug_return_type] || 'ListRefundsResponse'
+
+      # auth_names
+      auth_names = opts[:debug_auth_names] || ['bearerAuth']
+
+      new_options = opts.merge(
+        :operation => :"RefundsApi.list_refunds",
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type
+      )
+
+      data, status_code, headers = @api_client.call_api(:GET, local_var_path, new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: RefundsApi#list_refunds\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
       end
       return data, status_code, headers
     end
